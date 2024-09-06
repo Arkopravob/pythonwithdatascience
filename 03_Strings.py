d8c149776798e150bc486c82f2c538f8c700029e
arkapravo1995 <arka.bhuin@gmail.com>
2024-09-07 01:55:54 +0530

    slicing string

---
+++ 03_Strings.py
@@ -0,0 +1,16 @@
+name = "Arkopravo , pranab"
+print(name[0:6])
+print(len(name))
+
+#Slicing in String
+
+fruit = "Mango"
+print(fruit[0:4])
+print(fruit[1:3])
+print(fruit[:5])
+print(fruit[:len(fruit)-3])
+print(fruit[-3:-1])#negetive slicing
+nm ="Harry"
+print(len(nm))
+print(nm[-4:-2])
+#Loop through
