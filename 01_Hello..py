8f0bed11a29a98283ddc2790694b589eec3a2c23
arkapravo1995 <arka.bhuin@gmail.com>
2024-09-07 01:35:08 +0530

    looping through this string

--- 01_Hello..py
+++ 01_Hello..py
@@ -7,3 +7,9 @@ print(s)
 
 s1 = """hey coder hey i am enjoy coding"""
 print(s1)
+a ="apple"
+#loopin through this string
+
+print("lets use a for loop")
+for characters in a:
+    print(characters)
